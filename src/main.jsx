59793f3f1df01d4fdeab12a780ad53abe31fa0e8
arvind4501 <arvindjangirlpu@gmail.com>
2023-12-16 23:04:06 +0530

    implemed types od default and import and played around it.

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Hello from '../components/Greeting.jsx'
+import { Animals } from '../components/animals.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Hello />
+    <Animals />
   </React.StrictMode>,
 )
